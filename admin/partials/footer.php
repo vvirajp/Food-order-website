987bdc43749ebcc549b477c26d229d3f8068efe6
Viraj Patil <66640534+vvirajp@users.noreply.github.com>
2022-01-14 00:00:38 +0530

    Add files via upload

---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2020 All rights reserved, Food House. Developed By - <a href="https://www.linkedin.com/in/viraj-patil-280761170/"> Viraj Patil</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
